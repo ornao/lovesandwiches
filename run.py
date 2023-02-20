32dd132a9770f37813b8f37615973e22e25f2073
ornao <reynoldsorna@gmail.com>
2023-02-20 20:17:13 +0000

    add loop to request valid user data without restarting

--- run.py
+++ run.py
@@ -16,16 +16,20 @@ def get_sales_data():
     """
     get sales figures input from the user
     """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")
+    
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("data is valid!")
+            break
 
-    data_str = input("Enter your data here: ")
-   
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-
+    return sales_data
 
 def validate_data(values):
     """
@@ -41,6 +45,8 @@ def validate_data(values):
             )
     except ValueError as e: 
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
+    return True
 
-get_sales_data()
+data = get_sales_data()
